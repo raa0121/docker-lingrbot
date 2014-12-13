f8e1c24effa24c14ad11edf9cb4e6522bd40ff99
raa0121 <raa0121@gmail.com>
2014-12-14 04:02:10 +0900

    fix2

--- web.rb
+++ web.rb
@@ -18,7 +18,7 @@ end
 
 post '/:room' do
   content_type :text
-  docker = JSON.parse(prams[:payload])
+  docker = JSON.parse(params[:payload])
   repo = docker['repository']
   open("http://lingr.com/api/room/say?room=#{params[:room]}&bot=docker&text=[#{repo['repo_name']}]#{repo['repo_url']}&bot_verifier=1fe6460cd3a3e457b51c5f0c7f4023e4").read
 end
